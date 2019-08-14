71112999a48122bf3bc15ae2b1dded953b792566
kamalraj <kamalrajv3@gmail.com>
2019-08-14 12:58:15 +0530

    added table view helper.

---
+++ BooksArchiveDemo/Utility/TableViewHelper.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewHelper.swift
+//  BooksArchiveDemo
+//
+//  Created by kamalraj venkatesan on 14/08/19.
+//  Copyright © 2019 kamalraj venkatesan. All rights reserved.
+//
+
+import Foundation
