e83f9fdd4b521cc1125152b83f94b7bd7d307d5c
kamalraj <kamalrajv3@gmail.com>
2019-08-14 12:59:03 +0530

    TableViewHelper: added protocols for Generic Table View

--- BooksArchiveDemo/Utility/TableViewHelper.swift
+++ BooksArchiveDemo/Utility/TableViewHelper.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+
+public protocol ConfigurableCell {
+    associatedtype T
+    func configure(data: T)
+}
+
+public protocol CellConfigurator {
+    static var reuseId: String { get }
+    func configure(cell: UIView)
+}
+
+
+public class TableCellConfigurator<CellType: ConfigurableCell, DataType>: CellConfigurator where CellType.T == DataType, CellType: UITableViewCell {
+    public static var reuseId: String { return String(describing: CellType.self) }
+    
+    let data: DataType
+    
+    public init(data: DataType) {
+        self.data = data
+    }
+    
+    public func configure(cell: UIView) {
+        (cell as! CellType).configure(data: data)
+    }
+}
