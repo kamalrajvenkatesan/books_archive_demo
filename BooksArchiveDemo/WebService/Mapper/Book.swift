42a452eee46a40bea11b504d15faae1346c00dcb
kamalraj <kamalrajv3@gmail.com>
2019-08-14 13:24:58 +0530

    Book: Added codable for Book Response.

--- BooksArchiveDemo/WebService/Mapper/Book.swift
+++ BooksArchiveDemo/WebService/Mapper/Book.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+public struct Book: Codable {
+    let id: String
+    let book_title: String
+    let author_name: String?
+    let genre: String?
+    let publisher: String?
+    let author_country: String?
+    let sold_count: Int?
+    let image_url: String?
+}
