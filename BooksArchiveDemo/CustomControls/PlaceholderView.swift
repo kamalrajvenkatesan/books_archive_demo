7cc7a44309d28385b4ad6b366585468b6b47ed59
kamalraj <kamalrajv3@gmail.com>
2019-08-14 21:44:30 +0530

    added Placeholder view file

---
+++ BooksArchiveDemo/CustomControls/PlaceholderView.swift
@@ -0,0 +1,21 @@
+//
+//  PlaceholderView.swift
+//  BooksArchiveDemo
+//
+//  Created by kamalraj venkatesan on 14/08/19.
+//  Copyright © 2019 kamalraj venkatesan. All rights reserved.
+//
+
+import UIKit
+
+class PlaceholderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
