b85a5d2464472122e4105c752ab40558cc0d383b
kamalraj <kamalrajv3@gmail.com>
2019-08-14 13:02:44 +0530

    added table view cell class file

---
+++ BooksArchiveDemo/TableViewCell/BookThumbnailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookThumbnailTableViewCell.swift
+//  BooksArchiveDemo
+//
+//  Created by kamalraj venkatesan on 14/08/19.
+//  Copyright © 2019 kamalraj venkatesan. All rights reserved.
+//
+
+import UIKit
+
+class BookThumbnailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
