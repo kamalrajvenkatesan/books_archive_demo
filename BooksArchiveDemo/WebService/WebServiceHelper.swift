a8f12fe25be49fcea3bd7453513908faff5542f2
kamalraj <kamalrajv3@gmail.com>
2019-08-14 13:15:14 +0530

    WebServiceHelper: Added Error Enum with basic errors Added Api Route

--- BooksArchiveDemo/WebService/WebServiceHelper.swift
+++ BooksArchiveDemo/WebService/WebServiceHelper.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import Alamofire
+
+public struct RouteDetails {
+    var path: String
+    var method: HTTPMethod
+    var encoding: ParameterEncoding
+}
+
+public enum ApiRoute {
+    case getAllBooks
+    
+    public func getDetails() -> RouteDetails {
+        // Use Switch Statement for more than one api route
+        return RouteDetails(path: "http://android.jiny.mockable.io/getAll", method: .get, encoding: JSONEncoding.default)
+    }
+}
+
+
+public enum BookssAppErrors: Int {
+    case NoInternet = -1009
+    case NotFound = 404
+    case ValidationError = 422
+    case InternalServerError = 500
+    case Unknown = 0
+    
+    var getMessage: String {
+        switch self {
+        case .NoInternet:
+            return "Not able to reach, Kindly check the internet connection."
+        default:
+            return "Something went wrong"
+        }
+    }
+}
+
+
